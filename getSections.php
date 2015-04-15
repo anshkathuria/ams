c8a7d003879b4ce40d0b680d16f8a7b5383889b2
Abhishek Kathuria <anshkathuria@gmail.com>
2015-04-16 00:22:28 +0530

    Added Sections Display Restriction based upon database

--- getSections.php
+++ getSections.php
@@ -1,16 +1,40 @@
  <?php
+ session_start();
  if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest' && isset($_GET["year"]))
  {
      include_once("inc/sections.php");
 	 $year = $_GET["year"];
+	 		 
 	 ?>
      <option value="none" style="display:none" disabled selected>-- Section --</option>
      <?
 	 foreach($sections[$year] as $no=>$sec)
 	 {
-		?>
-        <option value="<?=$no ?>"><?=$sec ?></option>
-        <?
+		 if($_SESSION["role"]=="User" || false)
+		 {	 
+		 	 include_once("inc/dbconnect.php");
+			 if($getSec = $mysqli->query("SELECT `section-$year` from `ams-users` WHERE uname='".$_SESSION["logged"]."'"))
+			 {
+			 	if($getSecObj = $getSec->fetch_assoc())
+				{
+					$sectionsAllowed = json_decode((string)$getSecObj["section-$year"]);
+					
+					if(in_array($no,$sectionsAllowed))
+					{
+					?>
+					<option value="<?=$no ?>"><?=$sec ?></option>
+					<?
+					}
+				}
+			 }
+		 }
+		 else
+		 {
+		 
+		  ?>
+			<option value="<?=$no ?>"><?=$sec ?></option>
+		  <? 
+		 }
 	 }
  }
  else
